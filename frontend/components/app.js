10371761886a8a7b94b2a78e840de3b58ab9d452
x <elu.developer@gmail.com>
2020-06-11 21:45:12 +0200

    add favicon + initial design

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -2,6 +2,39 @@ import React from 'react'
 
 export default class App extends React.Component {
     render() {
-        return <h1>Hello!</h1>
+        return (
+            <div className="container">
+                <div className="row">
+                    <div id="logo">
+                        <img src="/icons/logo.png" alt="Helium logo"/>
+                        <span>Helium</span>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-3">
+                        <div className="nav">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quasi et maxime sunt molestias nulla laborum veniam officia, quibusdam animi debitis iste vitae cum. Voluptatem architecto sint quaerat nihil iusto.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quae enim, harum iure illo odit laboriosam rem ut quaerat voluptatum nisi voluptatem consectetur quisquam beatae vitae. Quasi alias neque aliquam?</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque neque fuga maiores eos similique minus enim saepe sapiente, ipsa aliquam delectus eaque veniam doloremque ullam sunt, nobis veritatis praesentium pariatur?</p>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="nav">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quasi et maxime sunt molestias nulla laborum veniam officia, quibusdam animi debitis iste vitae cum. Voluptatem architecto sint quaerat nihil iusto.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quae enim, harum iure illo odit laboriosam rem ut quaerat voluptatum nisi voluptatem consectetur quisquam beatae vitae. Quasi alias neque aliquam?</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque neque fuga maiores eos similique minus enim saepe sapiente, ipsa aliquam delectus eaque veniam doloremque ullam sunt, nobis veritatis praesentium pariatur?</p>
+                        </div></div>
+                    <div className="col-3">
+                        <div className="nav">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quasi et maxime sunt molestias nulla laborum veniam officia, quibusdam animi debitis iste vitae cum. Voluptatem architecto sint quaerat nihil iusto.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quae enim, harum iure illo odit laboriosam rem ut quaerat voluptatum nisi voluptatem consectetur quisquam beatae vitae. Quasi alias neque aliquam?</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque neque fuga maiores eos similique minus enim saepe sapiente, ipsa aliquam delectus eaque veniam doloremque ullam sunt, nobis veritatis praesentium pariatur?</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
     }
 }

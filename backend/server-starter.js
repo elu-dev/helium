df9a1c85420451a56703bf710a05c254bff15fb8
x <elu.developer@gmail.com>
2020-06-14 05:50:51 +0200

    add server atlas

--- backend/server-starter.js
+++ backend/server-starter.js
@@ -1,9 +1,6 @@
-const gateway = require('./services/gateway/gateway')
-const front = require('./services/front-server/front-server')
-const img_cdn = require('./services/img-cdn/img-cdn')
+const atlas = require('./shared/atlas')
 
 let log = (name, port) => console.log(`${name} started on port ${port}`)
 
-front.listen(8000, _ => log('front', 8000))
-gateway.listen(3000, _ => log('gateway', 3000))
-img_cdn.listen(3001, _ => log('img CDN', 3001))
+for (let server of atlas)
+    server.instance.listen(server.port, _ => log(server.name, server.port))
--- backend/shared/atlas.js
+++ backend/shared/atlas.js
@@ -1,5 +1,8 @@
-module.exports = {
-    '/': 'http://localhost:3001',
-    '/posts/': 'http://localhost:3002',
-    
-}
+const server = (name, url, port, instance) => ({ name, url, port, instance })
+
+module.exports = [
+    server('front',   'http://localhost:8000', 8000, require('../services/front-server/front-server')),
+    server('gateway', 'http://localhost:3000', 3000, require('../services/gateway/gateway')),
+    server('img CDN', 'http://localhost:3001', 3001, require('../services/img-cdn/img-cdn')),
+    // server('auth',    'http://localhost:3002', 3002, require('../services/auth/auth'))
+]

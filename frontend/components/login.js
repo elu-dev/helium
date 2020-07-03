4c445296cc1db3c7b3699cd7df44d01141cceb9a
x <elu.developer@gmail.com>
2020-07-03 13:56:58 +0200

    init login section

---
+++ frontend/components/login.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Post from './post'
+import STATE from './_global'
+import NewPost from './newpost'
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    // changer() {
+    //     STATE.posts[0].comment = 'omg it works!'
+    //     this.forceUpdate()
+    // }
+
+    render() {
+        return (
+            <div className="blob h_feed">
+                
+            </div>
+        )
+    }
+}
--- frontend/components/nav.js
+++ frontend/components/nav.js
@@ -5,9 +5,10 @@ const Nav = props => (
     <div className="col-3 d-none d-md-block">
         <div className="sticky-top h_stickfix">
             <div className="blob h_nav">
-                {   STATE.user.token ? <div className="h_nav_row">Profile</div>
-                                     : <div className="h_nav_row">Register</div>
-                } {!STATE.user.token ? <div className="h_nav_row">Login</div> : ''}
+                {
+                    STATE.user.token ? <div className="h_nav_row">Profile</div>
+                                     : <div className="h_nav_row">Login</div>
+                }
                 
                 <div className="h_nav_row">Settings</div>
             </div>
